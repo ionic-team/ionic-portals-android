92dedb97b7c8a149dcdc690c8170055b6fbd18e8
Dan Giralté (Ionic) <giralte@ionic.com>
2022-02-23 13:08:45 -0600

    initial commit

---
+++ IonicPortals/src/main/kotlin/io/ionic/portals/PortalMethod.kt
@@ -0,0 +1,4 @@
+package io.ionic.portals
+
+@Retention(AnnotationRetention.RUNTIME)
+annotation class PortalMethod(val topic: String = "")
---
+++ IonicPortals/src/main/kotlin/io/ionic/portals/Test.kt
@@ -0,0 +1,11 @@
+package io.ionic.portals
+
+import android.util.Log
+
+class Test {
+    companion object {
+        fun helloWorld() {
+            Log.d("DEBUG", "LOG LOG LOG")
+        }
+    }
+}
