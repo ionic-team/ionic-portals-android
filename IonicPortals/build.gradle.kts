c09a7a786ca81797132f92c66e9a0aeb5f96cef3
Carl Poole <carl@ionic.io>
2022-09-19 09:30:07 -0500

    chore: update dependency versions to capacitor 3.4.0 and liveupdates 0.1.1 (#12)

--- IonicPortals/build.gradle.kts
+++ IonicPortals/build.gradle.kts
@@ -38,8 +38,8 @@ android {
 dependencies {
     implementation(kotlin("reflect"))
 
-    api("com.capacitorjs:core:3.5.1")
-    compileOnly("io.ionic:liveupdates:0.0.7")
+    api("com.capacitorjs:core:3.7.0")
+    compileOnly("io.ionic:liveupdates:0.1.1")
 
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.5.0")
     implementation( "androidx.core:core-ktx:1.6.0")
