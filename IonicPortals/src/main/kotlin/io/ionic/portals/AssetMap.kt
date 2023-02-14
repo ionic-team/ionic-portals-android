af6ea85901597e07570755b799fa2865e883e252
Carl Poole <carl@ionic.io>
2023-02-14 15:01:08 -0600

    remove live updates for now

--- IonicPortals/src/main/kotlin/io/ionic/portals/AssetMap.kt
+++ IonicPortals/src/main/kotlin/io/ionic/portals/AssetMap.kt
@@ -1,14 +1,11 @@
 package io.ionic.portals
 
-import io.ionic.liveupdates.LiveUpdate
-
 class AssetMap(
     val name: String,
     val virtualPath: String,
     val prefix: String = "/assets",
     val path: String,
-    val startDir: String,
-    val liveUpdateConfig: LiveUpdate? = null
+    val startDir: String
 ) {
     val assetPath: String = "\$prefix\$path"
 
--- IonicPortals/src/main/kotlin/io/ionic/portals/PortalsRouteProcessor.kt
+++ IonicPortals/src/main/kotlin/io/ionic/portals/PortalsRouteProcessor.kt
@@ -14,16 +14,6 @@ class PortalsRouteProcessor(val context: Context, val assetMaps: LinkedHashMap<S
         if (assetMaps.containsKey(path)) {
             val assetMap = assetMaps[path]
             if (assetMap != null) {
-                // If Live Updates is configured and has been updated, route to that location instead
-                if (assetMap.liveUpdateConfig != null) {
-                    val liveUpdateDir = LiveUpdateManager.getLatestAppDirectory(context, assetMap.liveUpdateConfig.appId)
-                    if (liveUpdateDir != null) {
-                        processedRoute.path =  "$liveUpdateDir.path/$path"
-                        processedRoute.isAsset = false
-                        return processedRoute
-                    }
-                }
-
                 processedRoute.path = assetMap.assetPath
                 processedRoute.isAsset = true
             }
