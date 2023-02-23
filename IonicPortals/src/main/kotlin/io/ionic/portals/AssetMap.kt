e9f2a4bcd1c7cd170bebcc5f10cb298c81a7a839
Carl Poole <carl@ionic.io>
2023-02-23 08:23:37 -0600

    Trimmed the AssetMap file, startdir was the same as path

--- IonicPortals/src/main/kotlin/io/ionic/portals/AssetMap.kt
+++ IonicPortals/src/main/kotlin/io/ionic/portals/AssetMap.kt
@@ -1,35 +1,13 @@
 package io.ionic.portals
 
-class AssetMap {
-    val name: String
-    val virtualPath: String
-
+class AssetMap(
+    // A name for the asset map.
+    val name: String,
+    // A path to match via the web.
+    val virtualPath: String,
+    // The path of the shared assets within the Android assets directory.
     val path: String
-
-    var prefix: String = "/assets"
-    var startDir: String? = null
-
-    constructor(name: String, virtualPath: String, path: String) {
-        this.name = name
-        this.virtualPath = virtualPath
-        this.path = path
-    }
-
-    constructor(name: String, virtualPath: String, path: String, startDir: String) {
-        this.name = name
-        this.virtualPath = virtualPath
-        this.path = path
-        this.startDir = startDir
-    }
-
-    constructor(name: String, virtualPath: String, path: String, startDir: String, prefix: String) {
-        this.name = name
-        this.virtualPath = virtualPath
-        this.path = path
-        this.prefix = prefix
-        this.startDir = startDir
-    }
-
+) {
     fun getAssetPath(): String {
         return path
     }
