ba09549b6c359e824e36f7c600d90bc248760b03
Carl Poole <carl@ionic.io>
2023-03-07 10:02:05 -0600

    chore: add dokka docgen for API docs (#37)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,11 @@
+import org.jetbrains.dokka.base.DokkaBase
+import org.jetbrains.dokka.gradle.DokkaTask
+import org.jetbrains.dokka.base.DokkaBaseConfiguration
+
+plugins {
+    id("org.jetbrains.dokka") version "1.7.20"
+}
+
 buildscript {
     val kotlinVersion = "1.6.21"
     extra.apply {
@@ -11,11 +19,13 @@ buildscript {
             url = uri("https://plugins.gradle.org/m2/")
         }
     }
+
     dependencies {
         if (System.getenv("PORTALS_PUBLISH") == "true") {
             classpath("io.github.gradle-nexus:publish-plugin:1.1.0")
         }
 
+        classpath("org.jetbrains.dokka:dokka-base:1.7.20")
         classpath("com.android.tools.build:gradle:7.2.2")
         classpath(kotlin("gradle-plugin", version = kotlinVersion))
         classpath(kotlin("serialization", version = kotlinVersion))
@@ -32,6 +42,14 @@ allprojects {
     repositories {
         google()
         mavenCentral()
+    }
+
+    apply(plugin = "org.jetbrains.dokka")
+
+    tasks.withType<DokkaTask>().configureEach {
+        pluginConfiguration<DokkaBase, DokkaBaseConfiguration> {
+            footerMessage = "Created by Ionic"
+        }
     }
 }
 
