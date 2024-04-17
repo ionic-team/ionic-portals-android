655fddaefbda145378272e43d77579631cf3d995
Carl Poole <carl@ionic.io>
2024-04-17 10:33:00 -0500

    update dependencies prep for cap 6 release

--- IonicPortals/build.gradle.kts
+++ IonicPortals/build.gradle.kts
@@ -44,14 +44,14 @@ android {
 dependencies {
     implementation(kotlin("reflect"))
 
-    api("com.capacitorjs:core:6.0.0-rc.0")
-    compileOnly("io.ionic:liveupdates:0.4.1")
+    api("com.capacitorjs:core:6.0.0")
+    compileOnly("io.ionic:liveupdates:0.5.1")
 
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
-    implementation("androidx.core:core-ktx:1.10.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.8.0")
+    implementation("com.google.android.material:material:1.11.0")
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.4")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.0")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
