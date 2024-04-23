910ad51265ff6bc2bded94af0a56fc5b2fa5215b
Carl Poole <carl@ionic.io>
2024-04-23 15:09:10 -0500

    feat: Capacitor v6 Update (#60)

--- IonicPortals/build.gradle.kts
+++ IonicPortals/build.gradle.kts
@@ -10,11 +10,10 @@ if (System.getenv("PORTALS_PUBLISH") == "true") {
 
 android {
     namespace = "io.ionic.portals"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         minSdk = 22
-        targetSdk = 33
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
@@ -45,14 +44,14 @@ android {
 dependencies {
     implementation(kotlin("reflect"))
 
-    api("com.capacitorjs:core:[5.5.0,5.8.0)")
-    compileOnly("io.ionic:liveupdates:0.5.0")
+    api("com.capacitorjs:core:[6.0.0,6.1.0)")
+    compileOnly("io.ionic:liveupdates:0.5.1")
 
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
-    implementation("androidx.core:core-ktx:1.10.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.8.0")
+    implementation("com.google.android.material:material:1.11.0")
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.4")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.0")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
