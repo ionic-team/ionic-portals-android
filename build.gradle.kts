4e4886c7162ba2dc9ee346af6507dea4c1ebb638
Trevor Lambert <78672774+trevor-lambert@users.noreply.github.com>
2026-03-05 15:09:44 -0600

    chore(docs): update Dokka config for Kotlin 2 compatibility

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,5 @@
-import org.jetbrains.dokka.base.DokkaBase
-import org.jetbrains.dokka.gradle.DokkaTask
-import org.jetbrains.dokka.base.DokkaBaseConfiguration
-
 plugins {
-    id("org.jetbrains.dokka") version "1.7.20"
+    id("org.jetbrains.dokka") version "2.0.0"
     id("org.jetbrains.kotlin.plugin.serialization") version "2.2.20"
 }
 
@@ -26,7 +22,6 @@ buildscript {
             classpath("io.github.gradle-nexus:publish-plugin:1.1.0")
         }
 
-        classpath("org.jetbrains.dokka:dokka-base:1.7.20")
         classpath("com.android.tools.build:gradle:8.13.0")
         classpath("org.jetbrains.kotlin:kotlin-serialization:$kotlinVersion")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
@@ -46,9 +41,11 @@ allprojects {
 
     apply(plugin = "org.jetbrains.dokka")
 
-    tasks.withType<DokkaTask>().configureEach {
-        pluginConfiguration<DokkaBase, DokkaBaseConfiguration> {
-            footerMessage = "Created by Ionic"
+    extensions.configure<org.jetbrains.dokka.gradle.DokkaExtension> {
+        pluginsConfiguration {
+            html {
+                footerMessage.set("Created by Ionic")
+            }
         }
     }
 }
